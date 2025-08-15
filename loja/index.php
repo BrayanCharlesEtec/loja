92bacc2e1cd5f53dc7744302313001d4c1bbdcae
bryynnn <161508666+BrayanCharlesEtec@users.noreply.github.com>
2025-08-15 16:56:27 -0300

    Add files via upload

---
+++ loja/index.php
@@ -0,0 +1,16 @@
+<?php
+include 'cabecalho.php'
+?>
+<body>
+    <div class=conteiner>
+        <h1>Bem vindo ao meu site com crud</h1>
+        <h2>Brayan Charles & Arthur Prado</h2>
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+            <a href="#" type="button" class="btn btn-danger">Cadastrar</a>
+            <a href="#" type="button" class="btn btn-warning">Listar</a>
+        </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ loja/listar.php
@@ -0,0 +1,35 @@
+<?php
+    include 'cabecalho.php';
+?>
+<body>
+    <h1>Bem vindo ao 10 Sistema com CRUD</h1>
+    <h2>Anderson Vanin</h2>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</tho>
+                    <th scope="col">NOME</th>
+                    <th scope="col">PRECO</th>
+                    <th scope="col">QUANTIDADE</th>
+                    <th scope="col">OPÇÕES</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@ndo</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="#" type="button" class="btn btn-danger">Atualizar</a>
+                            <a href="#" type="button" class="htn lits-warning">Apagar</a>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
